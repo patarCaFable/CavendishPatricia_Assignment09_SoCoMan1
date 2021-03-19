5abaf557f8c0c83d60f70b82514b6b641b786494
Patricia Cavendish <patricia.cavendish@sinclair.edu>
2021-03-19 14:48:15 -0400

    Add project files.

---
+++ Source.cpp
@@ -0,0 +1,13 @@
+/*Patricia Cavendish
+* CIS 1202-501
+* March 21, 2021
+*/
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	cout << "Hello World" << endl;
+	system("PAUSE");
+	return 0;
+}
